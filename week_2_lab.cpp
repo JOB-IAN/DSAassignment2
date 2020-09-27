5765d0eb55f8b3d71bf022a0885cd38d9e8f8b07
scholarly-art <loganwaynea@gmail.com>
2020-09-27 21:38:09 +0300

    Rewritten the code using else if blocks instead of switch case. To note, using the EOF is very tricky when implemented with a loop. However, it is all a learning experience.

---
+++ week_2_lab.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+	char grade;
+	
+	int aCount, bCount, cCount, dCount, fCount;
+	aCount = 0;
+	bCount = 0;
+	cCount = 0;
+	dCount = 0;
+	fCount = 0;
+	cout << "Enter the letter grade, \n The EOF character to end input\n";
+	while( cin >> grade ){
+		grade = tolower(grade);
+		cout << "Enter the letter grade, \n The EOF character to end input\n";
+		
+		if(grade == 'a'){
+			aCount++;
+			continue;}
+		else if(grade == 'b'){	
+			bCount++;
+			continue;}
+		else if(grade == 'c'){	
+			cCount++;
+			continue;}
+		else if(grade == 'd'){	
+			dCount++;
+			continue;}
+		else if(grade == 'f'){
+			fCount++;
+			continue;}
+		else
+			cout << "Incorrect letter grade entered.\n Enter a new grade.\n";				
+		}
+		
+	cout << "The totals for each letter grade are :";
+			cout << "\nA : " << aCount;
+			cout << "\nB : " << bCount;
+			cout << "\nC : " << cCount;
+			cout << "\nD : " << dCount;
+			cout << "\nF : " << fCount;	
+	return 0;
+	}
